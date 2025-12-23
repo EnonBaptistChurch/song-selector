ce1972ce3eb2345b59783fcdf2584cfba0edb50b
David <davidmitchellfootball@googlemail.com>
2025-12-23 13:41:04 +0000

    trigger build and serve

--- song-selector/astro.config.mjs
+++ song-selector/astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 
+const devBase = process.env.NODE_ENV === 'development' ? '/' : '/song-selector/';
+
 export default defineConfig({
   integrations: [vue()],
-  site: 'https://enonbaptistchurch.github.io/song-selector/', // your GitHub Pages URL
-  base: '/song-selector/',                      // important for assets
-  build: { outDir: 'dist' },                   // default Astro build folder
+  site: 'https://enonbaptistchurch.github.io/song-selector/',
+  base: devBase,
+  build: { outDir: 'dist' },
 });
