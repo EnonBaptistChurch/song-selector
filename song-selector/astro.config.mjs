0c6f950e8d0cd560e0e7b71a94d72c9198d325d2
David <davidmitchellfootball@googlemail.com>
2025-12-30 12:06:38 +0000

    add tailwind

--- song-selector/astro.config.mjs
+++ song-selector/astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
+import tailwind from '@astrojs/tailwind';
 
 const devBase = process.env.NODE_ENV === 'development' ? '/' : '/song-selector/';
 
 export default defineConfig({
-  integrations: [vue()],
+  integrations: [vue(), tailwind()],
   site: 'https://enonbaptistchurch.github.io',
   base: devBase,
   build: { outDir: 'dist' },
